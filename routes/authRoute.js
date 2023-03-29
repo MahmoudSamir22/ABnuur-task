e666ee20797f27e7663adb5074f9ff9adb5978b0
Mahmoud Samir <menatosamir55@gmail.com>
2023-03-29 14:05:18 +0200

    adding validator

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 const { signUp, login, resendVerifyCode, verifyEmail } = require("../controllers/authController");
 
-router.post("/signUp", signUp);
+const {signupValidator} = require('../utils/validators/authValidator')
+
+router.post("/signUp", signupValidator, signUp);
 
 router.post("/login", login);
 
---
+++ utils/validators/authValidator.js
@@ -0,0 +1,22 @@
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const { body, check } = require("express-validator");
+const User = require("../../models/userModel");
+
+exports.signupValidator = [
+  body("userName").optional().isString().withMessage("UserName must be string"),
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please Enter a valid email").custom(async val =>{
+        const user = await User.findOne({where: {email: val}})
+        if(user){
+            throw new Error('This email already used please use another email')
+        }
+        return true
+    }),
+  body("password").notEmpty().withMessage("Password is required"),
+  role('role').optional(),
+  validatorMiddleware()
+];
+
